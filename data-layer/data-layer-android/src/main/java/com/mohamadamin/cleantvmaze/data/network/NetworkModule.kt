8550295594ea8c75a97e73b81766b066ec70b20d
mohamad-amin <mohamadamin@aut.ac.ir>
2017-05-01 20:04:39 +0430

    Added some network classes and NetworkModule for dagger

---
+++ data-layer/data-layer-android/src/main/java/com/mohamadamin/cleantvmaze/data/network/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.mohamadamin.cleantvmaze.data.network
+
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * @author MohamadAmin Mohamadi (mohammadi.mohamadamin@gmail.com) on 5/1/17.
+ * A Module for providing Network Apis and Gson
+ */
+@Singleton
+@Module
+class NetworkModule {
+
+    val URL = "http://api.tvmaze.com/"
+    private val GSON_VERSION = 1.0
+
+    @Provides
+    fun provideShowsAPI(retrofit: Retrofit): ShowsAPI {
+        return retrofit.create(ShowsAPI::class.java)
+    }
+
+    @Provides
+    fun provideRetrofit(converterFactory: Converter.Factory): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl(URL)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(converterFactory)
+                .build()
+    }
+
+    @Provides
+    fun provideConverterFactory(gson: Gson): Converter.Factory {
+        return GsonConverterFactory.create(gson)
+    }
+
+    @Provides
+    fun provideGson(): Gson {
+        return GsonBuilder().setVersion(GSON_VERSION)
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .create()
+    }
+
+}
---
+++ data-layer/data-layer-android/src/main/java/com/mohamadamin/cleantvmaze/data/network/ShowsAPI.kt
@@ -0,0 +1,26 @@
+package com.mohamadamin.cleantvmaze.data.network
+
+import com.mohamadamin.cleantvmaze.domain.entity.Episode
+import com.mohamadamin.cleantvmaze.domain.entity.Season
+import com.mohamadamin.cleantvmaze.domain.entity.Show
+import retrofit2.http.GET
+import rx.Observable
+
+/**
+ * @author MohamadAmin Mohamadi (mohammadi.mohamadamin@gmail.com) on 5/1/17.
+ */
+interface ShowsAPI {
+
+    @GET("/shows")
+    fun getShows(page: Int): Observable<List<Show>>
+
+    @GET("/shows/{id}")
+    fun getShow(showId: Int): Observable<Show>
+
+    @GET("/shows/{id}/episodes")
+    fun getEpisodes(showId: Int): Observable<List<Episode>>
+
+    @GET("/shows/{id}/seasons")
+    fun getSeasons(showId: Int): Observable<List<Season>>
+
+}
