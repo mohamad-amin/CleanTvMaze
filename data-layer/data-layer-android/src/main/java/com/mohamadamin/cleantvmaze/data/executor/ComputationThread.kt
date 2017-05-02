d9b3129521aab746a17cdb86a7472c78ca1935b3
mohamad-amin <mohamadamin@aut.ac.ir>
2017-05-02 23:51:29 +0430

    Added computation use case executor

---
+++ data-layer/data-layer-android/src/main/java/com/mohamadamin/cleantvmaze/data/executor/ComputationThread.kt
@@ -0,0 +1,13 @@
+package com.mohamadamin.cleantvmaze.data.executor
+
+import easymvp.executer.UseCaseExecutor
+import rx.schedulers.Schedulers
+
+/**
+ * @author MohamadAmin Mohamadi (mohammadi.mohamadamin@gmail.com) on 5/2/17.
+ */
+class ComputationThread: UseCaseExecutor {
+
+    override fun getScheduler() =
+            Schedulers.computation()
+}
