9e9f5fde98b16b16e587d5eefd777fb86df72047
mohamad-amin <mohamadamin@aut.ac.ir>
2017-05-06 17:15:05 +0430

    Added MainView

---
+++ presentation-layer/src/main/java/com/mohamadamin/cleantvmaze/page/main/MainPresenter.kt
@@ -0,0 +1,12 @@
+package com.mohamadamin.cleantvmaze.page.main
+
+import easymvp.RxPresenter
+
+/**
+ * @author MohamadAmin Mohamadi (mohammadi.mohamadamin@gmail.com) on 5/6/17.
+ */
+class MainPresenter: RxPresenter<MainView>() {
+
+
+
+}
